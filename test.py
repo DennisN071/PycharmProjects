fa9947ca7d18d3da4e1c21a2db40f759f8117f3d
Dennis <dennisnijgh@hotmail.com>
2021-07-31 16:49:16 +0200

    Dit is een test om te zien of het werkt.

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as npy
+from scipy import stats
+speed = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+
+mean = npy.mean(speed)
+median = npy.median(speed)
+mode = stats.mode(speed)
+standard_deviation = npy.std(speed)
+variance = npy.var(speed)
+
+print('The mean is', mean)
+print('The median is', median)
+print('The mode is', mode)
+print('The standard_deviation is', standard_deviation)
+print('The variance is', variance)
